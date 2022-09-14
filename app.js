70aba0d9c73968405897b14ded7cb0913f4e5954
danivent23122020 <dani.vent@orange.fr>
2022-09-14 08:17:11 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const allNavItems = document.querySelectorAll(".nav-item");
+const navbar = document.querySelector(".navbar-collapse");
+
+allNavItems.forEach((item) =>
+    item.addEventListener("click", () => {
+        navbar.classList.toggle("show");
+    })
+);
+
+//display btn
+window.addEventListener("scroll", function () {
+    let scroll = this.document.querySelector(".btn-scrollup");
+    scroll.classList.toggle("active", window.scrollY > 300);
+});
+
+// scrollup
+const btn = document.querySelector(".btn-scrollup");
+btn.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+    });
+});
